436893646147a35764b6b3e59b364b8cef23d736
Deshitha199 <IT20657628@my.sliit.lk>
2023-05-20 16:11:03 +0530

    Admin service

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const Order = require("../models/orderModal");
+const Admin = require("../models/admin");
 const { green } = require("colors");
+const genarateToken = require("../config/genarateToken");
+
 
 const getAllOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find();
@@ -41,7 +44,86 @@ const confirmOrder = asyncHandler(async (req, res) => {
   }
 });
 
+const adminAuth = asyncHandler(async (req, res) => {
+  
+  //getting body data
+  const { userName, password } = req.body;
+
+  console.log(userName,password);
+  //check if user available in database
+  const admin = await Admin.findOne({ userName });
+  if(!userName){
+    return res.status(400).send({ message: "Invalid User Name" });
+  }
+  if (!(await admin.matchPassword(password)))
+    return res.status(400).send({ message: "Incorrect Password " });
+
+
+  //if user available send response with matching password and genarate JWT token using user id
+  if (admin && (await admin.matchPassword(password))) {
+    res.status(200).json({
+      userName: admin.userName,
+      token: genarateToken(admin._id),
+    });
+  } else {
+    //send error message to frontend
+    console.log("Invalid user name or Password".red.bold);
+    res.status(400).json({
+      error: "Incorrect password !!!",
+    });
+    throw new error("Incorrect password !!!");
+  }
+});
+
+const addAdmin = asyncHandler(async (req, res) => {
+  //getting body data
+  const { userName, password} = req.body;
+
+  //backend validation for body data
+  if (!userName || !password) {
+    res.send(400);
+    throw new error("Please enter all the fields!!!");
+  }
+
+  //find if user exist with email and user name
+  const adminExist = await Admin.findOne({ userName });
+
+  //sending error message if user exist
+  if (adminExist) {
+    console.log("Admin already exist!!!".red.bold);
+    res.status(400).json({
+      error: "Admin already exist !!!",
+    });
+    throw new error("Admin already exist!!!");
+  }
+
+  //create new user in database
+  const admin = await Admin.create({
+    userName,
+    password,
+  });
+
+  //send response to frontend
+  if (admin) {
+    console.log("Registered!!!".green.bold);
+    res.status(201).json({
+      _id: admin._id,
+      userName: admin.userName,
+      token: genarateToken(admin._id),
+    });
+  } else {
+    //send error message to frontend
+    console.log("Failed to Register Admin !!!".red.bold);
+    res.status(400).json({
+      error: "Failed to Register Admin !!!",
+    });
+    throw new error("Failed to Register Admin !!!");
+  }
+});
+
 module.exports = {
   getAllOrders,
   confirmOrder,
+  adminAuth,
+  addAdmin,
 };
